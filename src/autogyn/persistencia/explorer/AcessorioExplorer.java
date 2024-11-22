c51f54ba2ec00bc781776529600264cdc532b6ff
Kaique Bahmad <kaiquebahmadt@gmail.com>
2024-11-22 14:15:30 -0300

    getAll do Acessorio feito

--- src/autogyn/persistencia/explorer/AcessorioExplorer.java
+++ src/autogyn/persistencia/explorer/AcessorioExplorer.java
@@ -1,32 +1,31 @@
 package autogyn.persistencia.explorer;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
 
+import autogyn.persistencia.entidades.Acessorio;
 import autogyn.persistencia.entidades.Servico;
 import autogyn.persistencia.ferramentas.ConexaoBD;
 import autogyn.persistencia.ferramentas.EasyQuery;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-public class ServicoExplorer {
+public class AcessorioExplorer {
 	private static Connection conn = ConexaoBD.getInstance();
 	
-	public static List<Servico> getAll() throws SQLException {
-        String sql = "select * from servico;";
-        List<Servico> servicos = new LinkedList<>();
+	public static List<Acessorio> getAll() throws SQLException {
+        String sql = "select * from acessorio;";
+        List<Acessorio> acessorios = new LinkedList<>();
+        System.out.println(conn);
         ResultSet rs = EasyQuery.exec(conn, sql);
         while(rs.next()) {
-            servicos.add(new Servico(rs));
+            acessorios.add(new Acessorio(rs));
         }
-        return servicos;
+        return acessorios;
 	}
 	
 	public static void main(String[] args) throws SQLException {
 		System.out.println(getAll());
 	}
-	
 }
