3d607566d1528aacdb88d2347924ab21d3399ae1
Danilo S Cunha <130316718+DaniloSk11@users.noreply.github.com>
2024-11-28 21:47:39 -0300

    Criado o Gateway de validações

--- src/main/java/com/pi/autogyn/servicos/ClienteService.java
+++ src/main/java/com/pi/autogyn/servicos/ClienteService.java
@@ -7,13 +7,7 @@ import com.pi.autogyn.persistencia.entidades.Cliente;
 import com.pi.autogyn.servicos.dto.CadastrarClienteDTO;
 
 public class ClienteService {
-	public static Long inserirCliente(CadastrarClienteDTO novoCliente) throws SQLException {
-	    if (novoCliente.getDdd() < 1 || novoCliente.getDdd() > 999) {
-            return null; // Retorna null se o DDD estiver fora do range
-        }
-	    if (novoCliente.getDdd2() != null && novoCliente.getDdd2() < 1 || novoCliente.getDdd2() > 999) {
-            return null; // Retorna null se o DDD estiver fora do range
-        }
+	public static Long inserirCliente(CadastrarClienteDTO novoCliente) throws SQLException {    
 		return ClienteDao.insert(new Cliente(novoCliente));
 	}
 }
